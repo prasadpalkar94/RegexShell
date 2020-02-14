85e3df7554cc849437e0c3b6f98c66810bf97899
prasadpalkar94 <prasadpalkar94@gmail.com>
2020-02-14 15:32:15 +0530

    ADDED:First Name Condition

--- regex.sh
+++ regex.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
+shopt -s extglob
 echo "Welcome To Regex Pattern"
+read word
+pattern1="^[A-Z]{1}[A-Za-z]{3,}$"
+
+if [[ $word =~ $pattern1 ]]
+then
+echo Valid
+else
+echo InValid
+fi
