2e8c300b1733b60bebae97ecbbf6b5295bbdca9d
prasadpalkar94 <prasadpalkar94@gmail.com>
2020-02-14 15:37:53 +0530

    ADDED:Last Name Condition

--- regex.sh
+++ regex.sh
@@ -2,9 +2,10 @@
 shopt -s extglob
 echo "Welcome To Regex Pattern"
 read word
-pattern1="^[A-Z]{1}[A-Za-z]{3,}$"
+first_name="^[A-Z]{1}[A-Za-z]{3,}$"
+last_name="^[A-Z]{1}[A-Za-z]{3,}$"
 
-if [[ $word =~ $pattern1 ]]
+if [[ $word =~ $last_name ]]
 then
 echo Valid
 else
